8d80f1a1b5b5864fa44066b9747189f8b049d2b1
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-12 23:03:48 +0200

    [W1-D4] DrawPyramid - Removed unnecessary code, simplified the rest, unfinished

--- week-01/day-4/src/DrawPyramid.java
+++ week-01/day-4/src/DrawPyramid.java
@@ -15,15 +15,18 @@ public class DrawPyramid {
         // The pyramid should have as many lines as the number was
 //        userInput 4   left 0  star 1  right 0
 
-
         Scanner scanner = new Scanner(System.in);
 
         int userInput = scanner.nextInt();
-        int lastLineLength = 1 + ((userInput - 1) * 2);
+        // int lastLineLength = userInput * 2 - 1;
+        // part of the math :)
 
         for (int lineNumber = 1; lineNumber <= userInput; lineNumber++) {
+            // int currentSpaceNeeded = userInput - lineNumber;
+            // int currentStarNeeded = lastLineLength - (currentSpaceNeeded * 2);
+            // did some math...
             int currentSpaceNeeded = userInput - lineNumber;
-            int currentStarNeeded = lastLineLength - (currentSpaceNeeded * 2);
+            int currentStarNeeded = lineNumber * 2 - 1;
 
             for (int leftSpace = 0; leftSpace < currentSpaceNeeded; leftSpace++) {
                 System.out.print(" ");
@@ -32,12 +35,6 @@ public class DrawPyramid {
             for (int star = 0; star < currentStarNeeded; star++) {
                 System.out.print("*");
             }
-
-
-            for (int rightSpace = 0; rightSpace < currentSpaceNeeded; rightSpace++) {
-                System.out.print(" ");
-            }
-
             System.out.println();
 
         }
