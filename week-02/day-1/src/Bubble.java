53f4ec997ca37978ba43a387b5648f3f7bceb9a8
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-16 16:07:42 +0200

    [W2-D1] Functions - Hard Ones - Bubble finished

--- week-02/day-1/src/Bubble.java
+++ week-02/day-1/src/Bubble.java
@@ -11,9 +11,37 @@ public class Bubble {
         System.out.println(Arrays.toString(sample1));
         //  should print [5, 9, 12, 24, 34]
 
-//        int[] sample2 = advancedBubble(new int[]{34, 12, 24, 9, 5}, true);
-//        System.out.println(Arrays.toString(sample2));
-//        //  should print [34, 24, 12, 9, 5]
+        int[] sample2 = advancedBubble(new int[]{34, 12, 24, 9, 5}, true);
+        System.out.println(Arrays.toString(sample2));
+        //  should print [34, 24, 12, 9, 5]
+
+        int[] sample3 = advancedBubble(new int[]{34, 12, 24, 9, 5}, false);
+        System.out.println(Arrays.toString(sample3));
+        //  should print [5, 9, 12, 24, 34]
+    }
+
+    private static int[] advancedBubble(int[] ints, boolean reverse) {
+        /**
+         * If the reverse parameter is false it calls the simple bubble() method
+         */
+
+        int[] result = ints;
+
+        if (reverse == false) {
+            result = bubble(ints);
+        } else {
+            for (int i = ints.length - 1; i >= 0; i--) {
+                for (int j = ints.length - 1; j >= 1; j--) {
+                    if (ints[j] > ints[j - 1]) {
+                        int temp = ints[j];
+
+                        ints[j] = ints[j - 1];
+                        ints[j - 1] = temp;
+                    }
+                }
+            }
+        }
+        return result;
     }
 
     private static int[] bubble(int[] ints) {
