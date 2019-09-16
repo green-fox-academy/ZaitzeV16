d9ed04095bb2e3c6c40159feeff3f6b515edf628
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-16 13:33:59 +0200

    [W2-D1] Functions - Exercises - Factorio added for-solution and changed int to long as return

--- week-02/day-1/src/Factorio.java
+++ week-02/day-1/src/Factorio.java
@@ -6,18 +6,30 @@ public class Factorio {
         // - Create a function called `factorio`
         //   that returns it's input's factorial
 
-        int someNumber = 5;
+        int someNumber = 20;
 
-        int factorialOf = factorio(someNumber);
+        long factorialRecursive = factorioRecursive(someNumber);
+        long factorialFor = factorioFor(someNumber);
 
-        System.out.println(factorialOf);
+        System.out.println(factorialRecursive);
+        System.out.println(factorialFor);
     }
 
-    private static int factorio(int num) {
+    private static long factorioFor(int number) {
+        long result = 1;
+
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+
+    private static long factorioRecursive(int num) {
         if (num == 1) {
             return 1;
         } else {
-            return num * (factorio(num - 1));
+            return num * (factorioRecursive(num - 1));
         }
     }
 }
