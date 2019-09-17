c5b7cad5900662523586a5742ea01bc6f8c29e62
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-17 18:25:31 +0200

    [W2-D2] Old exercises - Calculator completed

---
+++ week-02/day-2/src/Calculator.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String... args) {
+        // Create a simple calculator application which reads the parameters from the prompt
+        // and prints the result to the prompt.
+
+        // It should support the following operations,
+        // create a method named "calculate()":
+        // +, -, *, /, % and it should support two operands.
+        // The format of the expressions must be: {operation} {operand} {operand}.
+        // Examples: "+ 3 3" (the result will be 6) or "* 4 4" (the result will be 16)
+
+        // You can use the Scanner class
+        // It should work like this:
+
+        // Start the program
+        // It prints: "Please type in the expression:"
+        // Waits for the user input
+        // Print the result to the prompt
+        // Exit
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please type in the expression: ");
+        String userInput = scanner.nextLine();
+
+        System.out.println(calculate(userInput));
+    }
+
+    private static String calculate(String userInput) {
+        String result = "";
+        String[] items = userInput.split(" ");
+
+        double operand1 = Integer.parseInt(items[1]);
+        double operand2 = Integer.parseInt(items[2]);
+
+        // + - * / %
+        switch (items[0]) {
+            case "+":
+                result = String.valueOf(operand1 + operand2);
+                break;
+            case "-":
+                result = String.valueOf(operand1 - operand2);
+                break;
+            case "*":
+                result = String.valueOf(operand1 * operand2);
+                break;
+            case "/":
+                result = String.valueOf(operand1 / operand2);
+                break;
+            case "%":
+                result = String.valueOf(operand1 % operand2);
+                break;
+        }
+        return result;
+    }
+}
