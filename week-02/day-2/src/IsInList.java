6f8abedb0d683cc196d5996bbf68ebd0fb4921a6
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-17 17:14:06 +0200

    [W2-D2] Old exercises - IsInList completed

---
+++ week-02/day-2/src/IsInList.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class IsInList {
+    public static void main(String... args) {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16));
+        System.out.println(checkNums(list));
+
+        // Check if list contains all of the following elements: 4,8,12,16
+        // Create a method that accepts list as an input
+        // it should return "true" if it contains all, otherwise "false"
+
+    }
+
+    private static boolean checkNums(ArrayList<Integer> list) {
+        // 4,8,12,16
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(4);
+        nums.add(8);
+        nums.add(12);
+        nums.add(16);
+
+        boolean result = false;
+        if (list.containsAll(nums)) {
+            result = true;
+        }
+
+        return result;
+    }
+}
