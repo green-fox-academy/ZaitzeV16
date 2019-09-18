25adbe8ccfaa4df754486da9fb9f70dfec5df803
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-18 13:30:04 +0200

    [W2-D3] Drawing - HorizontalLines completed

---
+++ week-02/day-3/src/HorizontalLines.java
@@ -0,0 +1,73 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class HorizontalLines {
+    public static void mainDraw(Graphics graphics) {
+        // Create a function that draws a single line and takes 3 parameters:
+        // The x and y coordinates of the line's starting point and the graphics
+        // and draws a 50 long horizontal line from that point.
+        // Draw at least 3 lines with that function using a loop.
+        Random random = new Random();
+        int x, y;
+
+        for (int i = 0; i < 3000; i++) {
+            x = random.nextInt(WIDTH);
+            y = random.nextInt(HEIGHT);
+
+            graphics.setColor(new Color(random.nextInt(256),
+                                        random.nextInt(256),
+                                        random.nextInt(256)));
+
+            drawNHorizontalLines(x, y, graphics);
+        }
+
+    }
+
+    private static void drawNHorizontalLines(int x, int y, Graphics graphics) {
+        graphics.drawLine(x, y, x + 50, y);
+    }
+
+    // Don't touch the code below
+    //      sorry for that - ZaitzeV16
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+
+        //_______________________________________________________________
+        // set by ZaitzeV16                                           //|
+        // gets current maximum resolution minus the taskbar height   //|
+        int[] curRes = CurrentResolution.getCurrentMaxResolution();   //|
+        WIDTH = curRes[0];                                            //|
+        HEIGHT = curRes[1];                                           //|
+        //______________________________________________________________|
+
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+
+        //_______________________________________________________________
+        // set by ZaitzeV16                                           //|
+        // maximizes the canvas window by default                     //|
+        //      and places it to the middle                           //|
+        jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);               //|
+        //______________________________________________________________|
+
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
