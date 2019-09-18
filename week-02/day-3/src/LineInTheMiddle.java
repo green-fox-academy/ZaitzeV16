1a1eefe463dfcca5ab56903d65637542afdc1172
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-18 12:09:25 +0200

    [W2-D3] Drawing - LineInTheMiddle completed

--- week-02/day-3/src/LineInTheMiddle.java
+++ week-02/day-3/src/LineInTheMiddle.java
@@ -3,10 +3,15 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class ItsAMeTempletto {
+public class LineInTheMiddle {
     public static void mainDraw(Graphics graphics) {
-        // copy the task description here...
+        // draw a red horizontal line to the canvas' middle.
+        graphics.setColor(Color.RED);
+        graphics.drawLine(0, HEIGHT / 2, WIDTH, HEIGHT / 2);
 
+        // draw a green vertical line to the canvas' middle.
+        graphics.setColor(Color.GREEN);
+        graphics.drawLine(WIDTH / 2, 0, WIDTH / 2, HEIGHT);
     }
 
     // Don't touch the code below
