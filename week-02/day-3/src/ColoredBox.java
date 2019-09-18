aec9d1894fe863106a3d1c249c7fa8183ec995e6
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-18 12:14:59 +0200

    [W2-D3] Drawing - ColoredBox completed

---
+++ week-02/day-3/src/ColoredBox.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class ColoredBox {
+    public static void mainDraw(Graphics graphics) {
+        // Draw a box that has different colored lines on each edge.
+        graphics.setColor(Color.RED);
+        graphics.drawLine(WIDTH / 4, HEIGHT / 4, WIDTH * 3 / 4, HEIGHT / 4);
+
+        graphics.setColor(Color.GREEN);
+        graphics.drawLine(WIDTH * 3 / 4, HEIGHT / 4, WIDTH * 3 / 4, HEIGHT * 3 / 4);
+
+        graphics.setColor(Color.BLACK);
+        graphics.drawLine(WIDTH * 3 / 4, HEIGHT * 3 / 4, WIDTH / 4, HEIGHT * 3 / 4);
+
+        graphics.setColor(Color.BLUE);
+        graphics.drawLine(WIDTH / 4, HEIGHT * 3 / 4, WIDTH / 4, HEIGHT / 4);
+
+    }
+
+    // Don't touch the code below
+    //      sorry for that - ZaitzeV16
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+
+        //_______________________________________________________________
+        // set by ZaitzeV16                                           //|
+        // gets current maximum resolution minus the taskbar height   //|
+        int[] curRes = CurrentResolution.getCurrentMaxResolution();   //|
+        WIDTH = curRes[0];                                            //|
+        HEIGHT = curRes[1];                                           //|
+        //______________________________________________________________|
+
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+
+        //_______________________________________________________________
+        // set by ZaitzeV16                                           //|
+        // maximizes the canvas window by default                     //|
+        //      and places it to the middle                           //|
+        jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);               //|
+        //______________________________________________________________|
+
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
