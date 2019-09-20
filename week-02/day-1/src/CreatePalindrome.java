d5388ae0253e90a778afcf571ed6db56089f821e
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-20 20:26:02 +0200

    [W2-D1] Hard Ones - CreatePalindrome - applied google-java code style, added docstrings, fixed grammar

--- week-02/day-1/src/CreatePalindrome.java
+++ week-02/day-1/src/CreatePalindrome.java
@@ -1,54 +1,83 @@
 public class CreatePalindrome {
-    public static void main(String[] args) {
-//        Create a function named create palindrome following your current language's style guide. It should take a string, create a palindrome from it and then return it.
-//
-//                  Examples
-//          input	            output
-//            ""	              ""
-//        "greenfox"    	"greenfoxxofneerg"
-//          "123"           	"123321"
 
-        String sample1 = "";
-        String sample2 = "greenfox";
-        String sample3 = "123";
+  public static void main(String[] args) {
+    /*
+    Create a function named create palindrome following your current language's style guide.
+    It should take a string, create a palindrome from it and then return it.
+                   Examples
+          input	            output
+            ""	              ""
+        "greenfox"    	"greenfoxxofneerg"
+          "123"           	"123321"
 
-        String palindromSample1 = createPalindrom(sample1);
-        String palindromSample2 = createPalindrom(sample2);
-        String palindromSample3 = createPalindrom(sample3);
+     (2 solutions)
+     */
 
-        System.out.println(palindromSample1);
-        System.out.println(palindromSample2);
-        System.out.println(palindromSample3);
+    String sample1 = "";
+    String sample2 = "greenfox";
+    String sample3 = "123";
 
-        palindromSample1 = cPalindrom(sample1);
-        palindromSample2 = cPalindrom(sample2);
-        palindromSample3 = cPalindrom(sample3);
+    String palindromeSample1 = createPalindrome(sample1);
+    String palindromeSample2 = createPalindrome(sample2);
+    String palindromeSample3 = createPalindrome(sample3);
 
-        System.out.println(palindromSample1);
-        System.out.println(palindromSample2);
-        System.out.println(palindromSample3);
+    System.out.println(palindromeSample1);
+    System.out.println(palindromeSample2);
+    System.out.println(palindromeSample3);
+    //====================================================
+    palindromeSample1 = cPalindrome(sample1);
+    palindromeSample2 = cPalindrome(sample2);
+    palindromeSample3 = cPalindrome(sample3);
 
+    System.out.println(palindromeSample1);
+    System.out.println(palindromeSample2);
+    System.out.println(palindromeSample3);
+
+  }
+
+  private static String cPalindrome(String word) {
+    /**
+     * This function takes a String as parameter, creates
+     *  the reverse of it and returns the concatenated String.
+     *
+     * TLDR: creates a palindrome from the parameter
+     *
+     *
+     * @param   word - String to be palindromized
+     *
+     * @@return String - palindrome version of the parameter
+     */
+
+    String result = word;
+    String reversedWord;
+
+    StringBuilder stringBuilder = new StringBuilder(word);
+    reversedWord = stringBuilder.reverse().toString();
+    result += reversedWord;
+
+    return result;
+  }
+
+  private static String createPalindrome(String word) {
+    /**
+     * This function takes a String as parameter, creates
+     *  the reverse of it and returns the concatenated String.
+     *
+     * TLDR: creates a palindrome from the parameter
+     *
+     *
+     * @param   word - String to be palindromized
+     *
+     * @@return String - palindrome version of the parameter
+     */
+
+    String result = word;
+
+    // for solution
+    for (int i = word.length() - 1; i >= 0; i--) {
+      result += Character.toString(word.charAt(i));
     }
 
-    private static String cPalindrom(String word) {
-        String result = word;
-        String reversedWord;
-
-        StringBuilder stringBuilder = new StringBuilder(word);
-        reversedWord = stringBuilder.reverse().toString();
-        result += reversedWord;
-
-        return result;
-    }
-
-    private static String createPalindrom(String word) {
-        String result = word;
-
-        // for solution
-        for (int i = word.length() - 1; i >= 0; i--) {
-            result += Character.toString(word.charAt(i));
-        }
-
-        return result;
-    }
+    return result;
+  }
 }
