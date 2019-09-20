634e5c1c15a30331dc1f81e8ecab5bcd4cee761c
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-20 20:11:02 +0200

    [W2-D1] Hard Ones - AppendAFunc/Bubble - applied google-java code style, added docstring

--- week-02/day-1/src/AppendAFunc.java
+++ week-02/day-1/src/AppendAFunc.java
@@ -1,21 +1,32 @@
 public class AppendAFunc {
-    public static void main(String[] args) {
-        //  Create the usual class wrapper
-        //  and main method on your own.
 
-        // - Create a string variable named `typo` and assign the value `Chinchill` to it
-        // - Write a function called `appendAFunc` that gets a string as an input,
-        //   appends an 'a' character to its end and returns with a string
-        //
-        // - Print the result of `appendAFunc(typo)`
+  public static void main(String[] args) {
+    //  Create the usual class wrapper
+    //  and main method on your own.
 
-        String typo = "Chinchill";
+    // - Create a string variable named `typo` and assign the value `Chinchill` to it
+    // - Write a function called `appendAFunc` that gets a string as an input,
+    //   appends an 'a' character to its end and returns with a string
+    //
+    // - Print the result of `appendAFunc(typo)`
 
-        String correctedWord = appendAFunc(typo);
-        System.out.println(correctedWord);
-    }
+    String typo = "Chinchill";
 
-    private static String appendAFunc(String word2Correct) {
-        return word2Correct + "a";
-    }
+    String correctedWord = appendAFunc(typo);
+    System.out.println(correctedWord);
+  }
+
+  private static String appendAFunc(String word2Correct) {
+    /**
+     * This function takes a String as parameter and appends an 'a'
+     *    to the end.
+     *
+     *
+     * @param word2Correct - String
+     *
+     * @return parameter + 'a'
+     */
+
+    return word2Correct + "a";
+  }
 }
--- week-02/day-1/src/Bubble.java
+++ week-02/day-1/src/Bubble.java
@@ -1,62 +1,78 @@
 import java.util.Arrays;
 
 public class Bubble {
-    public static void main(String[] args) {
-        //  Create a function that takes a list of numbers as parameter
-        //  Returns a list where the elements are sorted in ascending numerical order
-        //  Make a second boolean parameter, if it's `true` sort that list descending
 
-        //  Example:
-        int[] sample1 = bubble(new int[]{34, 12, 24, 9, 5});
-        System.out.println(Arrays.toString(sample1));
-        //  should print [5, 9, 12, 24, 34]
+  public static void main(String[] args) {
+    //  Create a function that takes a list of numbers as parameter
+    //  Returns a list where the elements are sorted in ascending numerical order
+    //  Make a second boolean parameter, if it's `true` sort that list descending
 
-        int[] sample2 = advancedBubble(new int[]{34, 12, 24, 9, 5}, true);
-        System.out.println(Arrays.toString(sample2));
-        //  should print [34, 24, 12, 9, 5]
+    //  Example:
+    int[] sample1 = bubble(new int[]{34, 12, 24, 9, 5});
+    System.out.println(Arrays.toString(sample1));
+    //  should print [5, 9, 12, 24, 34]
 
-        int[] sample3 = advancedBubble(new int[]{34, 12, 24, 9, 5}, false);
-        System.out.println(Arrays.toString(sample3));
-        //  should print [5, 9, 12, 24, 34]
-    }
+    int[] sample2 = advancedBubble(new int[]{34, 12, 24, 9, 5}, true);
+    System.out.println(Arrays.toString(sample2));
+    //  should print [34, 24, 12, 9, 5]
 
-    private static int[] advancedBubble(int[] ints, boolean reverse) {
-        /**
-         * If the reverse parameter is false it calls the simple bubble() method
-         */
+    int[] sample3 = advancedBubble(new int[]{34, 12, 24, 9, 5}, false);
+    System.out.println(Arrays.toString(sample3));
+    //  should print [5, 9, 12, 24, 34]
+  }
 
-        int[] result = ints;
+  private static int[] advancedBubble(int[] ints, boolean reverse) {
+    /**
+     * This function applies reversed bubble sort on the integer array parameter.
+     * If the reverse parameter is false it calls the simple bubble() function.
+     *
+     *
+     * @param ints      int[] - to sort with bubble sort
+     * @param reverse   boolean - if false, calls the simple bubble() function
+     *
+     * @return (bubble)sorted int[] of the first parameter
+     *         if reverse parameter is true, the sort will be descending
+     */
 
-        if (reverse == false) {
-            result = bubble(ints);
-        } else {
-            for (int i = ints.length - 1; i >= 0; i--) {
-                for (int j = ints.length - 1; j >= 1; j--) {
-                    if (ints[j] > ints[j - 1]) {
-                        int temp = ints[j];
+    int[] result = ints;
 
-                        ints[j] = ints[j - 1];
-                        ints[j - 1] = temp;
-                    }
-                }
-            }
+    if (reverse == false) {
+      result = bubble(ints);
+    } else {
+      for (int i = ints.length - 1; i >= 0; i--) {
+        for (int j = ints.length - 1; j >= 1; j--) {
+          if (ints[j] > ints[j - 1]) {
+            int temp = ints[j];
+            ints[j] = ints[j - 1];
+            ints[j - 1] = temp;
+          }
         }
-        return result;
+      }
     }
+    return result;
+  }
 
-    private static int[] bubble(int[] ints) {
-        int[] result = ints;
+  private static int[] bubble(int[] ints) {
+    /**
+     * This function applies bubble sort on the integer array parameter.
+     *
+     *
+     * @param ints      int[] - to sort with bubble sort
+     *
+     * @return (bubble)sorted int[] of the parameter
+     */
 
-        for (int i = 0; i < ints.length; i++) {
-            for (int j = 0; j + 1 < (ints.length - i); j++) {
-                if (ints[j] > ints[j + 1]) {
-                    int temp = ints[j];
+    int[] result = ints;
 
-                    ints[j] = ints[j + 1];
-                    ints[j + 1] = temp;
-                }
-            }
+    for (int i = 0; i < ints.length; i++) {
+      for (int j = 0; j + 1 < (ints.length - i); j++) {
+        if (ints[j] > ints[j + 1]) {
+          int temp = ints[j];
+          ints[j] = ints[j + 1];
+          ints[j + 1] = temp;
         }
-        return result;
+      }
     }
+    return result;
+  }
 }
