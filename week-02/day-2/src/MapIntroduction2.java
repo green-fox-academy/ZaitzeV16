680c023b12168135afba46b7f79186bd7c247c0c
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-20 11:45:25 +0200

    [W2-D2] Data Structures - MapIntroduction2 - separated tasks, removed unnecessary variable, added comments

--- week-02/day-2/src/MapIntroduction2.java
+++ week-02/day-2/src/MapIntroduction2.java
@@ -5,6 +5,7 @@ import java.util.Map;
 public class MapIntroduction2 {
     public static void main(String[] args) {
         // Map introduction 2
+        // ======================================================================================
         // Create a map where the keys are strings and the
         // values are strings with the following initial values
         Map<String, String> isbn = new HashMap<>();
@@ -12,7 +13,6 @@ public class MapIntroduction2 {
         isbn.put("978-1-60309-459-7", "Lupus");
         isbn.put("978-1-60309-444-3", "Red Panda and Moon Bear");
         isbn.put("978-1-60309-461-0", "The Lab");
-
         /*
             Key	                Value
         978-1-60309-452-8	A Letter to Jo
@@ -20,7 +20,7 @@ public class MapIntroduction2 {
         978-1-60309-444-3	Red Panda and Moon Bear
         978-1-60309-461-0	The Lab
         */
-
+        // ======================================================================================
         // Print all the key-value pairs in the following format
         // A Letter to Jo (ISBN: 978-1-60309-452-8)
         // Lupus (ISBN: 978-1-60309-459-7)
@@ -33,32 +33,32 @@ public class MapIntroduction2 {
             System.out.println(")");
         }
         System.out.println();
-
+        // ======================================================================================
         // Remove the key-value pair with key 978-1-60309-444-3
         isbn.remove("978-1-60309-444-3");
-
+        // ======================================================================================
         // Remove the key-value pair with value The Lab
         // set iterator for desired hashmap
         Iterator<Map.Entry<String, String>> myIter = isbn.entrySet().iterator();
         while (myIter.hasNext()) {
-            // get iterator for current iteration
-            HashMap.Entry<String, String> myEntry = myIter.next();
-
-            if (myEntry.getValue() == "The Lab") {
-                isbn.remove(myEntry.getKey());
+            // myIter is the current iterator
+            // myIter.next() is the current iterator value (which is a Map)
+            // myIter.next().getValue() is the current iterator value's value
+            //      from the key-value pair (which is a value of the Map)
+            if (myIter.next().getValue() == "The Lab") {
+                isbn.remove(myIter.next().getKey());
                 break;
             }
         }
         /* OR
         for (HashMap.Entry<String, String> myEntry : isbn.entrySet()) {
-            // get iterator for current iteration
             if (myEntry.getValue() == "The Lab") {
                 isbn.remove(myEntry.getKey());
                 break;
             }
         }
          */
-
+        // ======================================================================================
         /*
         Add the following key-value pairs to the map
             Key             	Value
@@ -67,11 +67,11 @@ public class MapIntroduction2 {
         */
         isbn.put("978-1-60309-450-4", "They Called Us Enemy");
         isbn.put("978-1-60309-453-5", "Why Did We Trust Him?");
-
+        // ======================================================================================
         System.out.println("Print whether there is an associated value with key 478-0-61159-424-8 or not");
         isbn.containsKey("478-0-61159-424-8");
         System.out.println();
-
+        // ======================================================================================
         System.out.println("Print the value associated with key 978-1-60309-453-5");
         System.out.println(isbn.get("978-1-60309-453-5"));
     }
