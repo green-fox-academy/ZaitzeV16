6d1b1c759fb735002a2913b1cf548ad43bbf7a54
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-20 20:37:03 +0200

    [W2-D1] Arrays - DoubleItems - applied google-java code style

--- week-02/day-1/src/DoubleItems.java
+++ week-02/day-1/src/DoubleItems.java
@@ -1,17 +1,18 @@
 import java.util.Arrays;
 
 public class DoubleItems {
-    public static void main(String[] args) {
-        // - Create an array variable named `numList`
-        //   with the following content: `[3, 4, 5, 6, 7]`
-        // - Double all the values in the array
 
-        int[] numList = {3, 4, 5, 6, 7};
-        System.out.println(Arrays.toString(numList));
+  public static void main(String[] args) {
+    // Create an array variable named `numList`
+    //   with the following content: `[3, 4, 5, 6, 7]`
 
-        for (int i = 0; i < numList.length; i++) {
-            numList[i] *= 2;
-        }
-        System.out.println(Arrays.toString(numList));
+    int[] numList = {3, 4, 5, 6, 7};
+    System.out.println(Arrays.toString(numList));
+
+    // Double all the values in the array
+    for (int i = 0; i < numList.length; i++) {
+      numList[i] *= 2;
     }
+    System.out.println(Arrays.toString(numList));
+  }
 }
