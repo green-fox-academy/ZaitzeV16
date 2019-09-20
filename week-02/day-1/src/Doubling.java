b911562776f3c5fcd933b388da548e75df9e0b3f
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-20 20:41:30 +0200

    [W2-D1] Arrays - Doubling - applied google-java code style, added docstring

--- week-02/day-1/src/Doubling.java
+++ week-02/day-1/src/Doubling.java
@@ -1,16 +1,27 @@
 public class Doubling {
-    public static void main(String[] args) {
-        // - Create an integer variable named `baseNum` and assign the value `123` to it
-        // - Create a function called `doubling` that doubles it's input parameter
-        //      and returns with an integer
-        // - Print the result of `doubling(baseNum)`
 
-        int baseNum = 123;
-        int doubledNum = doubling(baseNum);
-        System.out.println(doubledNum);
-    }
+  public static void main(String[] args) {
+    // - Create an integer variable named `baseNum` and assign the value `123` to it
+    // - Create a function called `doubling` that doubles it's input parameter
+    //      and returns with an integer
+    // - Print the result of `doubling(baseNum)`
 
-    private static int doubling(int num) {
-        return num * 2;
-    }
+    int baseNum = 123;
+    int doubledNum = doubling(baseNum);
+    System.out.println(doubledNum);
+  }
+
+  private static int doubling(int num) {
+    /**
+     * This function takes an int as parameter and
+     *  returns the doubled value.
+     *
+     *
+     * @param num int
+     *
+     * @return doubled parameter
+     */
+
+    return num * 2;
+  }
 }
