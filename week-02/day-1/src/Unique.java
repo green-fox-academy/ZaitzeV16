439cf081ee8d7f488dcd461779867c6ed8a3fcf3
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-20 22:21:50 +0200

    [W2-D1] Hard Ones - Unique - applied google-java code style, added docstrings, renamed function

--- week-02/day-1/src/Unique.java
+++ week-02/day-1/src/Unique.java
@@ -2,20 +2,29 @@ import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class Unique {
-    public static void main(String[] args) {
-        //  Create a function that takes a list of numbers as a parameter
-        //  Returns a list of numbers where every number in the list occurs only once
 
-        //  Example
-        int[] sample1 = {1, 11, 34, 11, 52, 61, 1, 34};
-        int[] result = unique(sample1);
-        System.out.println(Arrays.toString(result));
-        //  should print: `[1, 11, 34, 52, 61]`
-    }
+  public static void main(String[] args) {
+    //  Create a function that takes a list of numbers as a parameter
+    //  Returns a list of numbers where every number in the list occurs only once
 
-    private static int[] unique(int[] ints) {
-        int[] result = IntStream.of(ints).distinct().toArray();
+    //  Example
+    int[] sample1 = {1, 11, 34, 11, 52, 61, 1, 34};
+    int[] result = filterUnique(sample1);
+    System.out.println(Arrays.toString(result));
+    //  should print: `[1, 11, 34, 52, 61]`
+  }
 
-        return result;
-    }
+  private static int[] filterUnique(int[] ints) {
+    /**
+     * This function takes an int[] as parameter and filters it.
+     * Leaves only the unique elements.
+     *
+     *
+     * @param   ints[] - the int[] to be filtered
+     *
+     * @return unique-filtered parameter
+     */
+
+    return IntStream.of(ints).distinct().toArray();
+  }
 }
