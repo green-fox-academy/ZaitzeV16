fa74a6447cc5de6ea0def78180119dda53a16b3e
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-20 20:34:59 +0200

    [W2-D1] Arrays - DiagonalMatrix - applied google-java code style, renamed iterators

--- week-02/day-1/src/DiagonalMatrix.java
+++ week-02/day-1/src/DiagonalMatrix.java
@@ -1,32 +1,33 @@
 import java.util.Arrays;
 
 public class DiagonalMatrix {
-    public static void main(String[] args) {
-        // - Create (dynamically) a two dimensional array
-        //   with the following matrix. Use a loop!
-        //
-        //   1 0 0 0
-        //   0 1 0 0
-        //   0 0 1 0
-        //   0 0 0 1
-        //
-        // - Print this two dimensional array to the output
 
-        int n = 5;
-        // nXn matrix
-        int[][] myMatrix = new int[n][n];
+  public static void main(String[] args) {
+    // - Create (dynamically) a two dimensional array
+    //   with the following matrix. Use a loop!
+    //
+    //   1 0 0 0
+    //   0 1 0 0
+    //   0 0 1 0
+    //   0 0 0 1
+    //
+    // - Print this two dimensional array to the output
 
-        for (int i = 0; i < myMatrix.length; i++) {
-            for (int j = 0; j < myMatrix[i].length; j++) {
-                if (i == j) {
-                    myMatrix[i][j] = 1;
-                } else {
-                    myMatrix[i][j] = 0;
-                }
-            }
-            System.out.println(Arrays.toString(myMatrix[i]));
+    int n = 5;
+    // nXn matrix
+    int[][] myMatrix = new int[n][n];
+
+    for (int row = 0; row < myMatrix.length; row++) {
+      for (int column = 0; column < myMatrix[row].length; column++) {
+        if (row == column) {
+          myMatrix[row][column] = 1;
+        } else {
+          myMatrix[row][column] = 0;
         }
-        System.out.println("______________________________");
-        System.out.println(Arrays.deepToString(myMatrix));
+      }
+      System.out.println(Arrays.toString(myMatrix[row]));
     }
+    System.out.println("______________________________");
+    System.out.println(Arrays.deepToString(myMatrix));
+  }
 }
