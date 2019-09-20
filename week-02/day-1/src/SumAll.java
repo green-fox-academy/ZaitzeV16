b21f1780c690201e9d9bd4615029bed8120183f7
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-20 22:16:44 +0200

    [W2-D1] Arrays - SumAll - applied google-java code style

--- week-02/day-1/src/SumAll.java
+++ week-02/day-1/src/SumAll.java
@@ -1,13 +1,14 @@
 import java.util.stream.IntStream;
 
 public class SumAll {
-    public static void main(String[] args) {
-        // - Create an array variable named `ai`
-        //   with the following content: `[3, 4, 5, 6, 7]`
-        // - Print the sum of the elements in `ai`
 
-        int[] ai = {3, 4, 5, 6, 7};
-        int sum = IntStream.of(ai).sum();
-        System.out.println(sum);
-    }
+  public static void main(String[] args) {
+    // - Create an array variable named `ai`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Print the sum of the elements in `ai`
+
+    int[] ai = {3, 4, 5, 6, 7};
+    int sum = IntStream.of(ai).sum();
+    System.out.println(sum);
+  }
 }
