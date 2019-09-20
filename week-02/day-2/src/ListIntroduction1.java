884826f10796142a530a9b3e73c5fba1d2c26475
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-20 10:52:15 +0200

    [W2-D2] Data Structures - ListIntroduction1 - fixed semantic issue, separated tasks

--- week-02/day-2/src/ListIntroduction1.java
+++ week-02/day-2/src/ListIntroduction1.java
@@ -5,36 +5,36 @@ public class ListIntroduction1 {
         // List introduction 1
         // We are going to play with lists.
         // Feel free to use the built-in methods where possible.
-        //
+        // =================================================================
         // Create an empty list which will contain names (strings)
         ArrayList<String> names = new ArrayList<>();
         System.out.println();
-
+        // =================================================================
         System.out.println("Print out the number of elements in the list");
         System.out.println(names.size());
         System.out.println();
-
+        // =================================================================
         // Add William to the list
         names.add("William");
-
+        // =================================================================
         System.out.println("Print out whether the list is empty or not");
         System.out.println(names.isEmpty());
         System.out.println();
-
+        // =================================================================
         // Add John to the list
         names.add("John");
-
+        // =================================================================
         // Add Amanda to the list
         names.add("Amanda");
-
+        // =================================================================
         System.out.println("Print out the number of elements in the list");
         System.out.println(names.size());
         System.out.println();
-
+        // =================================================================
         System.out.println("Print out the 3rd element");
         System.out.println(names.get(2));
         System.out.println();
-
+        // =================================================================
         System.out.println("Iterate through the list and print out each name");
         //      William
         //      John
@@ -43,7 +43,7 @@ public class ListIntroduction1 {
             System.out.println(name);
         }
         System.out.println();
-
+        // =================================================================
         System.out.println("Iterate through the list and print with numbers");
         //      1. William
         //      2. John
@@ -53,18 +53,18 @@ public class ListIntroduction1 {
             System.out.println(names.get(i));
         }
         System.out.println();
-
+        // =================================================================
         // Remove the 2nd element
         names.remove(1);
-
+        // =================================================================
         System.out.println("Iterate through the list in a reversed order and print out each name");
         //      Amanda
         //      William
-        for (String name : names) {
-            System.out.println(name);
+        for (int i = (names.size() - 1); i >= 0; i--) {
+            System.out.println(names.get(i));
         }
         System.out.println();
-
+        // =================================================================
         // Remove all elements
         names.clear();
     }
