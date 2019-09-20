6c29cf4ef35f1ab9a96ae0e08f7f83799fb37242
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-20 13:28:45 +0200

    [W2-D2] Data Structures - ProductBase - rewrote some tasks for simplicity, fixed semantic issues, separated tasks, added some comments

--- week-02/day-2/src/ProductBase.java
+++ week-02/day-2/src/ProductBase.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -9,9 +11,8 @@ public class ProductBase {
         We are going to represent our products in a map where the keys are
         strings representing the product's name and the values are numbers
         representing the product's price.
-
+        // ======================================================================================
         Create a map with the following key-value pairs.
-
             Product name (key)	Price (value)
                   Eggs          	200
                   Milk          	200
@@ -28,30 +29,45 @@ public class ProductBase {
         products.put("Apples", 150);
         products.put("Bread", 50);
         products.put("Chicken", 550);
-
+        // ======================================================================================
         System.out.println("How much is the fish?");
         System.out.println(products.get("Fish"));
         System.out.println();
-
+        // ======================================================================================
         System.out.println("What is the most expensive product?");
+        // this code snippet looks up all the products
+        //  for the highest value
         int myMax = Collections.max(products.values());
+        ArrayList<String> keysOfMax = new ArrayList<>();
+
         for (HashMap.Entry<String, Integer> myIter : products.entrySet()) {
             if (myIter.getValue() == myMax) {
-                System.out.println(myIter.getKey() + " is the most expensive.");
-                break;
+                keysOfMax.add(myIter.getKey());
             }
         }
-        System.out.println();
 
+        // there will be at least 1 item in keysOfMax if products is not empty
+        if (keysOfMax.size() == 1) {
+            System.out.println(keysOfMax.get(0) + " is the most expensive.");
+        } else {
+            System.out.println("The most expensive products are: ");
+            System.out.println(keysOfMax.toString());
+        }
+        System.out.println();
+        // ======================================================================================
         System.out.println("What is the average price?");
         int sum = 0;
         for (int value : products.values()) {
             sum += value;
         }
-        double avg = sum / products.values().size();
+
+        // set decimal precision to 2 decimal places
+        BigDecimal bigDecimal = new BigDecimal((double) sum / products.size());
+        BigDecimal avg = bigDecimal.setScale(2, RoundingMode.HALF_UP);
+
         System.out.println("The average price is: " + avg);
         System.out.println();
-
+        // ======================================================================================
         System.out.println("How many products' price is below 300?");
         int counter = 0;
         for (int value : products.values()) {
@@ -61,30 +77,27 @@ public class ProductBase {
         }
         System.out.println(counter);
         System.out.println();
-
+        // ======================================================================================
         System.out.println("Is there anything we can buy for exactly 125?");
-        int myCounter = 0;
-        for (HashMap.Entry<String, Integer> myIter : products.entrySet()) {
-            if (myIter.getValue() == 125) {
-                myCounter++;
-                System.out.println(myIter.getKey() + " is exactly 125");
+        if (products.containsValue(125)) {
+            for (HashMap.Entry<String, Integer> myIter : products.entrySet()) {
+                if (myIter.getValue() == 125) {
+                    System.out.println(myIter.getKey() + " is exactly 125");
+                }
             }
-        }
-        if (myCounter == 0) {
+        } else {
             System.out.println("There are not items with the exact price of 125.");
         }
         System.out.println();
-
+        // ======================================================================================
         System.out.println("What is the cheapest product?");
-        int min = 0;
+
+        System.out.println("The cheapest product(s) is(are): ");
+        int cheapestValue = Collections.min(products.values());
         for (HashMap.Entry<String, Integer> myIter : products.entrySet()) {
-            if (min == 0) {
-                min = myIter.getValue();
-            }
-            if (myIter.getValue() < min) {
-                min = myIter.getValue();
+            if (myIter.getValue().equals(cheapestValue)) {
+                System.out.println("\t" + myIter.getKey());
             }
         }
-        System.out.println("The cheapest is: " + min);
     }
 }
