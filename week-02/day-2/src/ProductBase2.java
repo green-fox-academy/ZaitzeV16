fc143bf8ac3fe87a926dcfacbe497287cf104e4f
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-20 13:31:16 +0200

    [W2-D2] Data Structures - ProductBase2 - separated tasks, fixed semantic issues in print statements

--- week-02/day-2/src/ProductBase2.java
+++ week-02/day-2/src/ProductBase2.java
@@ -7,7 +7,7 @@ public class ProductBase2 {
         We are going to represent our products in a map where the keys are
         strings representing the product's name and the values are numbers
         representing the product's price.
-
+        // ======================================================================================
         Create a map with the following key-value pairs.
         Product name (key)	Price (value)
               Eggs	            200
@@ -24,24 +24,25 @@ public class ProductBase2 {
         products.put("Apples", 150);
         products.put("Bread", 50);
         products.put("Chicken", 550);
-
+        // ======================================================================================
         /*
         Create an application which solves the following problems.
             Which products cost less than 201? (just the name)
             Which products cost more than 150? (name + price)
          */
+        // ======================================================================================
         System.out.println("Which products cost less than 201? (just the name)");
         for (HashMap.Entry<String, Integer> myIter : products.entrySet()) {
             if (myIter.getValue() < 201) {
-                System.out.println(myIter.getKey() + " is more expensive than 201");
+                System.out.println(myIter.getKey() + " is less expensive than 201");
             }
         }
         System.out.println();
-
+        // ======================================================================================
         System.out.println("Which products cost more than 150? (name + price)");
         for (HashMap.Entry<String, Integer> myIter : products.entrySet()) {
             if (myIter.getValue() > 150) {
-                System.out.println(myIter.getKey() + " is cheaper than 150 and it costs: " + myIter.getValue());
+                System.out.println(myIter.getKey() + " is more expensive than 150 and it costs: " + myIter.getValue());
             }
         }
     }
