eb6744535575b4e21cd12199f9363dcfbbf0181e
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-20 20:48:46 +0200

    [W2-D1] Functions - Factorio - applied google-java code style, added docstrings

--- week-02/day-1/src/Factorio.java
+++ week-02/day-1/src/Factorio.java
@@ -1,35 +1,55 @@
 public class Factorio {
-    public static void main(String[] args) {
-        //  Create the usual class wrapper
-        //  and main method on your own.
 
-        // - Create a function called `factorio`
-        //   that returns it's input's factorial
+  public static void main(String[] args) {
+    //  Create the usual class wrapper
+    //  and main method on your own.
 
-        int someNumber = 20;
+    // - Create a function called `factorio`
+    //   that returns it's input's factorial
 
-        long factorialRecursive = factorioRecursive(someNumber);
-        long factorialFor = factorioFor(someNumber);
+    int someNumber = 20;
 
-        System.out.println(factorialRecursive);
-        System.out.println(factorialFor);
+    long factorialRecursive = factorioRecursive(someNumber);
+    long factorialFor = factorioFor(someNumber);
+
+    System.out.println(factorialRecursive);
+    System.out.println(factorialFor);
+  }
+
+  private static long factorioFor(int number) {
+    /**
+     * This function takes a long and calculates
+     *  the factorial of it (using for loop).
+     *
+     *
+     * @param number - long to be factored
+     *
+     * @return factored parameter (long)
+     */
+
+    long result = 1;
+
+    for (int i = 1; i <= number; i++) {
+      result *= i;
     }
+    return result;
+  }
 
-    private static long factorioFor(int number) {
-        long result = 1;
+  private static long factorioRecursive(int num) {
+    /**
+     * This function takes a long and calculates
+     *  the factorial of it (using recursion).
+     *
+     *
+     * @param number - long to be factored
+     *
+     * @return factored parameter (long)
+     */
 
-        for (int i = 1; i <= number; i++) {
-            result *= i;
-        }
-
-        return result;
+    if (num == 1) {
+      return 1;
+    } else {
+      return num * (factorioRecursive(num - 1));
     }
-
-    private static long factorioRecursive(int num) {
-        if (num == 1) {
-            return 1;
-        } else {
-            return num * (factorioRecursive(num - 1));
-        }
-    }
+  }
 }
