8dddf84f4a34420df3a71f7fa107fa388a5bd9c6
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-20 19:23:20 +0200

    [W2-D1] Hard Ones - SearchPalindrome - completed

--- week-02/day-1/src/SearchPalindrome.java
+++ week-02/day-1/src/SearchPalindrome.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
 
 public class SearchPalindrome {
-    public static void main(String[] args) {
+
+  public static void main(String[] args) {
 //        Create a function named search palindrome following your current language's style guide.
 //        It should take a string, search for palindromes that at least 3 characters long and return
 //        a list with the found palindromes.
@@ -13,68 +14,75 @@ public class SearchPalindrome {
 //                   "racecar"                          	["racecar", "aceca", "cec"]
 //                      ""                                          	[]
 
-        String sample1 = "";
-        //output []
+    String sample1 = "";
+    // output []
 
-        String sample2 = "dog goat dad duck doodle never";
-        // output ["og go", "g g", " dad ", "dad", "d d", "dood", "eve"]
+    String sample2 = "dog goat dad duck doodle never";
+    // output ["og go", "g g", " dad ", "dad", "d d", "dood", "eve"]
 
-        String sample3 = "apple";
-        //output []
+    String sample3 = "apple";
+    // output []
 
-        String sample4 = "racecar";
-        // output ["racecar", "aceca", "cec"]
+    String sample4 = "racecar";
+    // output ["racecar", "aceca", "cec"]
 
-        String[] palindromSample1 = searchPalindrome(sample1);
-        String[] palindromSample2 = searchPalindrome(sample2);
-        String[] palindromSample3 = searchPalindrome(sample3);
-        String[] palindromSample4 = searchPalindrome(sample4);
+    String[] palindromeSample1 = searchPalindrome(sample1);
+    String[] palindromeSample2 = searchPalindrome(sample2);
+    String[] palindromeSample3 = searchPalindrome(sample3);
+    String[] palindromeSample4 = searchPalindrome(sample4);
 
-        System.out.println(Arrays.toString(palindromSample1));
-        System.out.println(Arrays.toString(palindromSample2));
-        System.out.println(Arrays.toString(palindromSample3));
-        System.out.println(Arrays.toString(palindromSample4));
+    System.out.println(Arrays.toString(palindromeSample1));
+    System.out.println(Arrays.toString(palindromeSample2));
+    System.out.println(Arrays.toString(palindromeSample3));
+    System.out.println(Arrays.toString(palindromeSample4));
+  }
+
+  private static boolean checkIfPalindrome(String stringToCheck) {
+    /**
+     * This function takes a String as parameter and checks
+     *  whether it is a palindrome or not.
+     *
+     *
+     * @param stringToCheck - String to analyze
+     *
+     * @return boolean - true is the parameter is a palindrome,
+     *                   false is not
+     */
+
+    StringBuilder stringBuilder = new StringBuilder(stringToCheck);
+    String reversedWord = stringBuilder.reverse().toString();
+
+    if (stringToCheck.equals(reversedWord)) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    private static String[] searchPalindrome(String word) {
-        String[] result = new String[0];
-        String tempStr = "";
-        boolean isPalindrom = false;
+  private static String[] searchPalindrome(String word) {
+    /**
+     * This function takes a String as parameter, looks for
+     *  every consecutive substring and calls the 'checkIfPalindrome'
+     *  function to decide whether the substrings are palindromes or not.
+     *
+     *
+     * @param word - String to analyze
+     *
+     * @return String[] - every palindrome the parameter contains
+     */
 
-        for (int i = 1; i < (word.length() - 1); i++) {
-//            if (isPalindrom) {
-//                result = Arrays.copyOf(result, result.length + 1);
-//                result[result.length - 1] = tempStr;
-//            }
-            tempStr = "";
-//            isPalindrom = false;
-            // TODO: palindroms with even length are still missing
-            
-            for (int j = 1; (j <= i) && j <= (word.length() - i - 1); j++) {
-                if (word.charAt(i - j) == word.charAt(i + j)) {
-                    // +1 correction because .substring (begin, end-1)
-                    tempStr = word.substring(i - j, i + j + 1);
-                    result = Arrays.copyOf(result, result.length + 1);
-                    result[result.length - 1] = tempStr;
-//                    isPalindrom = true;
-                } else {
-                    break;
-                }
-            }
-//            for (int j = 1; )
+    String[] palindromes = new String[0];
+    int wordLen = word.length();
 
-            // like this, we only get the unique palindromes with odd length
-            // we get racecar, but no cec, nor aceca
-            // for (int j = 1; (j <= i) && j <= (word.length() - i - 1); j++) {
-            // if (word.charAt(i - j) == word.charAt(i + j)) {
-            // +1 correction because .substring (begin, end-1)
-            // tempStr = word.substring(i - j, i + j + 1);
-            // isPalindrom = true;
-            // } else {
-            // break;
-            // }
-            // }
+    for (int currLen = 3; currLen <= wordLen; currLen++) {
+      for (int start = 0; start <= (wordLen - currLen); start++) {
+        String currSubStr = word.substring(start, start + currLen);
+        if (checkIfPalindrome(currSubStr)) {
+          palindromes = Arrays.copyOf(palindromes, palindromes.length + 1);
+          palindromes[palindromes.length - 1] = currSubStr;
         }
-        return result;
+      }
     }
+    return palindromes;
+  }
 }
