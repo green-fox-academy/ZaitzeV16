f5158b886741d4ff517c69eba9f594420e9f9d06
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-20 22:14:49 +0200

    [W2-D1] Functions - Sum - applied google-java code style, added docstrings, added recursive solution

--- week-02/day-1/src/Sum.java
+++ week-02/day-1/src/Sum.java
@@ -1,21 +1,53 @@
 public class Sum {
-    public static void main(String[] args) {
-        // Create the usual class wrapper and main method on your own.
 
-        // Write a function called `sum` that returns the sum of numbers
-        // from zero to the given parameter
+  public static void main(String[] args) {
+    // Create the usual class wrapper and main method on your own.
 
-        int x = 200;
-        int sum = sumOfRange(x);
-        System.out.println(sum);
+    // Write a function called `sum` that returns the sum of numbers
+    // from zero to the given parameter
+
+    int x = 200;
+    int sum = sumOfRange(x);
+    System.out.println(sum);
+
+    int sumRec = sumOfRangeRecursive(x);
+    System.out.println(sumRec);
+  }
+
+  private static int sumOfRange(int x) {
+    /**
+     * This function takes an int as parameter
+     *  and sums the numbers below them, including the parameter.
+     *
+     *
+     * @param   x - int, 0 => x range to be summed
+     *
+     * @return int, the sum of the numbers below parameter (included)
+     */
+
+    int sum = 0;
+    for (int i = 0; i <= x; i++) {
+      sum += i;
     }
+    return sum;
+  }
 
-    private static int sumOfRange(int x) {
-        int sum = 0;
-        for (int i = 0; i <= x; i++) {
-            sum += i;
-        }
-
-        return sum;
+  private static int sumOfRangeRecursive(int x) {
+    /**
+     * This function takes an int as parameter
+     *  and sums the numbers below them, including the parameter
+     *  (recursively).
+     *
+     *
+     * @param   x - int, 0 => x range to be summed
+     *
+     * @return int, the sum of the numbers below parameter (included)
+     */
+    
+    if (x == 0) {
+      return 0;
+    } else {
+      return x + sumOfRangeRecursive(x - 1);
     }
+  }
 }
