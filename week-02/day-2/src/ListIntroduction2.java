a00f341f2c5e30a75a7bb2d151ce2c48e50a9bac
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-20 11:03:02 +0200

    [W2-D2] Data Structures - ListIntroduction2 - simplified code, separated tasks, fixed semantic issue

--- week-02/day-2/src/ListIntroduction2.java
+++ week-02/day-2/src/ListIntroduction2.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ListIntroduction2 {
     public static void main(String[] args) {
         // List introduction 2
+        // =================================================================
+        // List A = friuts
+        // List B = copyOfFruits
         // Create a list ('List A') which contains the following values
         //     Apple, Avocado, Blueberries, Durian, Lychee
         ArrayList<String> fruits = new ArrayList<>();
@@ -12,23 +16,20 @@ public class ListIntroduction2 {
         fruits.add("Blueberries");
         fruits.add("Durian");
         fruits.add("Lychee");
-
+        // =================================================================
         // Create a new list ('List B') with the values of List A
-        ArrayList<String> copyOfFruit = new ArrayList<>();
-        for (String fruit : fruits) {
-            copyOfFruit.add(fruit);
-        }
-
+        ArrayList<String> copyOfFruit = new ArrayList<>(fruits);
+        // =================================================================
         System.out.println("Print out whether List A contains Durian or not");
         System.out.println(fruits.contains("Durian"));
         System.out.println();
-
+        // =================================================================
         // Remove Durian from List B
         copyOfFruit.remove("Durian");
-
+        // =================================================================
         // Add Kiwifruit to List A after the 4th element
         fruits.add(4, "Kiwifruit");
-
+        // =================================================================
         System.out.println("Compare the size of List A and List B");
         System.out.print("Size of List A: " + fruits.size() + "\t|\t");
         System.out.println("Size of List B: " + copyOfFruit.size());
@@ -39,18 +40,24 @@ public class ListIntroduction2 {
             System.out.println("No");
         }
         System.out.println();
-
+        // =================================================================
         System.out.println("Get the index of Avocado from List A");
         System.out.println(fruits.indexOf("Avocado"));
         System.out.println();
-
+        // =================================================================
         System.out.println("Get the index of Durian from List B");
-        System.out.println(copyOfFruit.indexOf("Durian"));
+        int temp = copyOfFruit.indexOf("Durian");
+        if (temp == -1) {
+            System.out.println("List B does not contain Durian");
+        } else {
+            System.out.println("The index of Durian is: " + temp);
+        }
         System.out.println();
 
+        // =================================================================
         // Add Passion Fruit and Pomelo to List B in a single statement
         copyOfFruit.addAll(Arrays.asList("Passion Fruit", "Pomelo"));
-
+        // =================================================================
         System.out.println("Print out the 3rd element from List A");
         System.out.println(fruits.get(2));
     }
