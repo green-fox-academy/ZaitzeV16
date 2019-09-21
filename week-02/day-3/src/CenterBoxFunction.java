e1e3f9d5a531a5f4d12fcc69d3c4fb0e4c1002cf
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-21 13:29:54 +0200

    [W2-D3] Drawing - CenteredBoxFunction - restructured code and separated to functions, added docstrings, added 1 parameter to the loop, applied code style

--- week-02/day-3/src/CenterBoxFunction.java
+++ week-02/day-3/src/CenterBoxFunction.java
@@ -5,87 +5,145 @@ import java.util.Random;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class CenterBoxFunction {
-    public static void mainDraw(Graphics graphics) {
-        // create a function that draws one square and takes 2 parameters:
-        // the square size and the graphics
-        // and draws a square of that size to the center of the canvas.
-        // draw 3 squares with that function.
-        // avoid code duplication.
-        Random random = new Random();
 
-        int sizeMax = 0;
-        if (WIDTH >= HEIGHT) {
-            sizeMax = HEIGHT + 1;
-        } else {
-            sizeMax = WIDTH + 1;
-        }
-
-        for (int i = 0; i < 4000; i++) {
-            graphics.setColor(new Color(random.nextInt(256),
-                                        random.nextInt(256),
-                                        random.nextInt(256)));
-
-            int width = random.nextInt(sizeMax);
-
-            centeredSquare(width, graphics);
-        }
+  public static void mainDraw(Graphics graphics) {
+    // create a function that draws one square and takes 2 parameters:
+    // the square size and the graphics
+    // and draws a square of that size to the center of the canvas.
+    // draw 3 squares with that function.
+    // avoid code duplication.
+    int numberOfSquares = 400;
+    int sizeMax;
+    if (WIDTH >= HEIGHT) {
+      sizeMax = HEIGHT;
+    } else {
+      sizeMax = WIDTH;
     }
 
-    private static void centeredSquare(int width, Graphics graphics) {
-        int height = width;
-
-        int[] widths = {(WIDTH - width) / 2,
-                (WIDTH + width) / 2,
-                (WIDTH + width) / 2,
-                (WIDTH - width) / 2,
-                (WIDTH - width) / 2};
-        int[] heights = {(HEIGHT - height) / 2,
-                (HEIGHT - height) / 2,
-                (HEIGHT + height) / 2,
-                (HEIGHT + height) / 2,
-                (HEIGHT - height) / 2};
-
-        graphics.drawPolygon(widths, heights, 4);
+    for (int i = 0; i < numberOfSquares; i++) {
+      createRandomCenteredSquare(sizeMax, graphics);
     }
+  }
 
-    // Don't touch the code below
-    //      sorry for that - ZaitzeV16
-    static int WIDTH = 320;
-    static int HEIGHT = 320;
+  private static void createRandomCenteredSquare(int sizeMax, Graphics graphics) {
+    /**
+     * This function calls:
+     *                      setRandomColor      - generate and set random color
+     *                      generateRandomSize  - generate random width (= height)
+     *                      drawCenteredSquare  - draw centered square (with width)
+     *  with proper arguments.
+     *
+     *
+     * @param sizeMax       int      - max value of  width (= height)
+     * @param graphics      Graphics - needed for the canvas (to draw, set color)
+     */
 
-    public static void main(String[] args) {
-        JFrame jFrame = new JFrame("Drawing");
-        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        ImagePanel panel = new ImagePanel();
+    Random random = new Random();
 
-        //_______________________________________________________________
-        // set by ZaitzeV16                                           //|
-        // gets current maximum resolution minus the taskbar height   //|
-        int[] curRes = CurrentResolution.getCurrentMaxResolution();   //|
-        WIDTH = curRes[0];                                            //|
-        HEIGHT = curRes[1];                                           //|
-        //______________________________________________________________|
+    setRandomColor(random, graphics);
+    int width = generateRandomSize(sizeMax, random);
+    drawCenteredSquare(width, graphics);
+  }
 
-        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        jFrame.add(panel);
-        jFrame.setLocationRelativeTo(null);
+  private static int generateRandomSize(int sizeMax, Random random) {
+    /**
+     * This function creates a random integer within 0 and sizeMax parameter.
+     *
+     *
+     * @param sizeMax   int      - bound of the random int
+     * @param random    Random   - instance of the Random class
+     *
+     * @return int - random number between 0(included) and sizeMax parameter (excluded)
+     */
 
-        //_______________________________________________________________
-        // set by ZaitzeV16                                           //|
-        // maximizes the canvas window by default                     //|
-        //      and places it to the middle                           //|
-        jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);               //|
-        //______________________________________________________________|
+    return random.nextInt(sizeMax);
+  }
 
-        jFrame.setVisible(true);
-        jFrame.pack();
+  private static void setRandomColor(Random random, Graphics graphics) {
+    /**
+     * This function sets the next to-be-drawn item's color to random).
+     *
+     *
+     * @param random    Random   - instance of the Random class
+     * @param graphics  Graphics - needed for the canvas (to draw, set color)
+     */
+
+    graphics.setColor(new Color(
+      random.nextInt(256),
+      random.nextInt(256),
+      random.nextInt(256)));
+  }
+
+  private static void drawCenteredSquare(int width, Graphics graphics) {
+    /**
+     * This function draws a canvas-centered square using the width parameter (/2).
+     *
+     *
+     * @param width     int      - the diameter of the square
+     * @param graphics  Graphics - needed for the canvas (to draw, set color)
+     */
+
+    int height = width;
+
+    // calculate square's corners' X coordinates
+    int[] widths = {
+      (WIDTH - width) / 2,
+      (WIDTH + width) / 2,
+      (WIDTH + width) / 2,
+      (WIDTH - width) / 2,
+      (WIDTH - width) / 2,
+    };
+    // calculate square's corners' Y coordinates
+    int[] heights = {
+      (HEIGHT - height) / 2,
+      (HEIGHT - height) / 2,
+      (HEIGHT + height) / 2,
+      (HEIGHT + height) / 2,
+      (HEIGHT - height) / 2,
+    };
+
+    graphics.drawPolygon(widths, heights, 4);
+  }
+
+  // Don't touch the code below
+  //      sorry for that - ZaitzeV16
+  static int WIDTH = 320;
+  static int HEIGHT = 320;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
+
+    //_______________________________________________________________
+    // set by ZaitzeV16                                           //|
+    // gets current maximum resolution minus the taskbar height   //|
+    int[] curRes = CurrentResolution.getCurrentMaxResolution();   //|
+    WIDTH = curRes[0];                                            //|
+    HEIGHT = curRes[1];                                           //|
+    //______________________________________________________________|
+
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
+    jFrame.setLocationRelativeTo(null);
+
+    //_______________________________________________________________
+    // set by ZaitzeV16                                           //|
+    // maximizes the canvas window by default                     //|
+    //      and places it to the middle                           //|
+    jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);               //|
+    //______________________________________________________________|
+
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
+
+  static class ImagePanel extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
     }
-
-    static class ImagePanel extends JPanel {
-        @Override
-        protected void paintComponent(Graphics graphics) {
-            super.paintComponent(graphics);
-            mainDraw(graphics);
-        }
-    }
+  }
 }
