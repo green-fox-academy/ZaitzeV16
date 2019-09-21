ec39c2340b308aefd8c288a8cd01c4129e0570bd
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-21 17:12:25 +0200

    [W2-D3] Drawing - PositionSquare - applied code style, added functions and docstrings, added starter parameter

--- week-02/day-3/src/PositionSquare.java
+++ week-02/day-3/src/PositionSquare.java
@@ -5,93 +5,127 @@ import java.util.Random;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class PositionSquare {
-    public static void mainDraw(Graphics graphics) {
-        // create a function that draws one square and takes 3 parameters:
-        // the x and y coordinates of the square's top left corner and the graphics
-        // and draws a 50x50 square from that point.
-        // draw 3 squares with that function.
-        // avoid code duplication.
-        Random random = new Random();
 
-        // initialize variables
-        int width = 50;
-        int height = width;
-        int x, y;
+  public static void mainDraw(Graphics graphics) {
+    // create a function that draws one square and takes 3 parameters:
+    // the x and y coordinates of the square's top left corner and the graphics
+    // and draws a 50x50 square from that point.
+    // draw 3 squares with that function.
+    // avoid code duplication.
 
-        for (int i = 0; i < 400; i++) {
-            graphics.setColor(new Color(random.nextInt(256),
-                    random.nextInt(256),
-                    random.nextInt(256)));
+    // initialize starter parameters
+    int numberOfSquares = 400;
+    int width = 50;
 
-            x = random.nextInt(WIDTH - width + 1);
-            y = random.nextInt(HEIGHT - height + 1);
+    drawSquares(width, numberOfSquares, graphics);
+  }
 
-            drawMySquare(x, y, width, graphics);
-        }
+  private static void drawSquares(int width, int numberOfSquares, Graphics graphics) {
+    /**
+     * This function handles:
+     *          - calling the proper functions
+     *                  setRandomColor
+     *                  drawMySquare
+     *          - randomization of the starting point of squares
+     *
+     * The first parameter will be the width (and height) of the squares,
+     *  the second is the number of the squares, and the third is needed
+     *  to draw them.
+     * The color of the squares are randomized for each.
+     *
+     *
+     * @param width             int      - the width and height of the squares
+     * @param numberOfSquares   int      - the number of squares
+     * @param graphics          Graphics - needed for the canvas (to draw and set color)
+     */
+
+    Random random = new Random();
+
+    int height = width;
+    int x, y;
+
+    for (int i = 0; i < numberOfSquares; i++) {
+      setRandomColor(graphics);
+
+      x = random.nextInt(WIDTH - width + 1);
+      y = random.nextInt(HEIGHT - height + 1);
+
+      drawMySquare(x, y, width, graphics);
     }
+  }
 
-    private static void drawMySquare(int x, int y, int width, Graphics graphics) {
-        int height = width;
-        int[] widths = new int[5];
-        int[] heights = new int[5];
+  private static void drawMySquare(int x, int y, int width, Graphics graphics) {
+    /**
+     * This function draws a square starting from
+     *  the x and y parameters, using the width parameter.
+     *
+     *
+     * @param x             int      - X coordinate of the the TOP LEFT point of the square
+     * @param x             int      - Y coordinate of the the TOP LEFT point of the square
+     * @param width         int      - the width and height of the square
+     * @param graphics      Graphics - needed for the canvas (to draw and set color)
+     */
 
-        // set new coordinates, according to new width and height to stay inbound
-        widths[0] = x;
-        heights[0] = y;
+    graphics.drawRect(x, y, width, width);
+  }
 
-        widths[1] = widths[0] + width;
-        heights[1] = heights[0];
+  private static void setRandomColor(Graphics graphics) {
+    /**
+     * This function sets the next to-be-drawn item's color to random.
+     *
+     *
+     * @param random    Random   - instance of the Random class
+     * @param graphics  Graphics - needed for the canvas (to set color)
+     */
+    Random random = new Random();
 
-        widths[2] = widths[1];
-        heights[2] = heights[0] + height;
+    graphics.setColor(new Color(
+            random.nextInt(256),
+            random.nextInt(256),
+            random.nextInt(256)
+        )
+    );
+  }
 
-        widths[3] = widths[0];
-        heights[3] = heights[2];
+  // Don't touch the code below
+  //      sorry for that - ZaitzeV16
+  static int WIDTH = 320;
+  static int HEIGHT = 320;
 
-        widths[4] = widths[0];
-        heights[4] = heights[0];
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
 
-        graphics.drawPolygon(widths, heights, 4);
+    //_______________________________________________________________
+    // set by ZaitzeV16                                           //|
+    // gets current maximum resolution minus the taskbar height   //|
+    int[] curRes = CurrentResolution.getCurrentMaxResolution();   //|
+    WIDTH = curRes[0];                                            //|
+    HEIGHT = curRes[1];                                           //|
+    //______________________________________________________________|
+
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
+    jFrame.setLocationRelativeTo(null);
+
+    //_______________________________________________________________
+    // set by ZaitzeV16                                           //|
+    // maximizes the canvas window by default                     //|
+    //      and places it to the middle                           //|
+    jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);               //|
+    //______________________________________________________________|
+
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
+
+  static class ImagePanel extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
     }
-
-    // Don't touch the code below
-    //      sorry for that - ZaitzeV16
-    static int WIDTH = 320;
-    static int HEIGHT = 320;
-
-    public static void main(String[] args) {
-        JFrame jFrame = new JFrame("Drawing");
-        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        ImagePanel panel = new ImagePanel();
-
-        //_______________________________________________________________
-        // set by ZaitzeV16                                           //|
-        // gets current maximum resolution minus the taskbar height   //|
-        int[] curRes = CurrentResolution.getCurrentMaxResolution();   //|
-        WIDTH = curRes[0];                                            //|
-        HEIGHT = curRes[1];                                           //|
-        //______________________________________________________________|
-
-        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        jFrame.add(panel);
-        jFrame.setLocationRelativeTo(null);
-
-        //_______________________________________________________________
-        // set by ZaitzeV16                                           //|
-        // maximizes the canvas window by default                     //|
-        //      and places it to the middle                           //|
-        jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);               //|
-        //______________________________________________________________|
-
-        jFrame.setVisible(true);
-        jFrame.pack();
-    }
-
-    static class ImagePanel extends JPanel {
-        @Override
-        protected void paintComponent(Graphics graphics) {
-            super.paintComponent(graphics);
-            mainDraw(graphics);
-        }
-    }
+  }
 }
