3cdeaf913144594978233d13d294275477c3e86f
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-21 17:28:20 +0200

    [W2-D3] Drawing - PurpleSteps - applied code style, added functions and docstrings, added starter parameter

--- week-02/day-3/src/PurpleSteps.java
+++ week-02/day-3/src/PurpleSteps.java
@@ -4,105 +4,117 @@ import java.awt.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class PurpleSteps {
-    public static void mainDraw(Graphics graphics) {
-        // Reproduce this:
-        // [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/assets/r3.png]
 
-        // initialize variables
-        int width = 30;
-        int height = width;
-        // width and height difference
-        int difference = 6;
+  public static void mainDraw(Graphics graphics) {
+    // Reproduce this:
+    // [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/assets/r3.png]
 
-        int x0 = 50;
-        int y0 = 50;
-        int x, y;
+    // initialize variables
+    int width = 30;
+    // width and height difference between the black and purple squares
+    int border = 6;
+    int numberOfSteps = 20;
 
-        // this is for the black stairs
-        for (int stair = 0; stair < 20; stair++) {
-            // for the black stair
-            graphics.setColor(Color.BLACK);
+    // stair staring point
+    int x0 = 50;
+    int y0 = 50;
 
-            x = x0 + (stair * width);
-            y = y0 + (stair * height);
+    createStaircase(x0, y0, width, border, numberOfSteps, graphics);
+  }
 
-            fillRect(x, y, width, graphics);
+  private static void createStaircase(
+      int startX, int startY,
+      int width, int border,
+      int numberOfSteps,
+      Graphics graphics) {
+    /**
+     * This function creates a descending purple staircase with a black border.
+     *
+     *
+     * @param startX            int      - X coordinate of the TOP LEFT point of the staircase
+     * @param startY            int      - Y coordinate of the TOP LEFT point of the staircase
+     * @param width             int      - width and height of one stair-step
+     * @param border            int      - width of the black border
+     * @param numberOfSteps     int      - number of stair-steps
+     * @param graphics          Graphics - needed for the canvas (to draw and set color)
+     */
+    
+    int height = width;
+    int x, y;
 
+    for (int stair = 0; stair < numberOfSteps; stair++) {
+      // for the black stair
+      graphics.setColor(Color.BLACK);
 
-            // for the purple stair
-            graphics.setColor(new Color(128, 0, 128));
+      x = startX + (stair * width);
+      y = startY + (stair * height);
 
-            x = x + (difference / 2);
-            y = y + (difference / 2);
+      fillSquare(x, y, width, graphics);
 
-            fillRect(x, y, (width - difference), graphics);
+      // for the purple stair
+      graphics.setColor(new Color(128, 0, 128));
 
-        }
+      x = x + (border / 2);
+      y = y + (border / 2);
+
+      fillSquare(x, y, (width - border), graphics);
     }
+  }
 
-    public static void fillRect(int x, int y, int width, Graphics graphics) {
-        int height = width;
+  public static void fillSquare(int x, int y, int width, Graphics graphics) {
+    /**
+     * This function draws a square starting from
+     *  the x and y parameters, using the width parameter.
+     *
+     *
+     * @param x             int      - X coordinate of the the TOP LEFT point of the square
+     * @param x             int      - Y coordinate of the the TOP LEFT point of the square
+     * @param width         int      - the width and height of the square
+     * @param graphics      Graphics - needed for the canvas (to draw and set color)
+     */
 
-        int[] widths = new int[5];
-        int[] heights = new int[5];
+    graphics.fillRect(x, y, width, width);
+  }
 
-        // set new coordinates, according to new width and height to stay inbound
-        widths[0] = x;
-        heights[0] = y;
+  // Don't touch the code below
+  //      sorry for that - ZaitzeV16
+  static int WIDTH = 320;
+  static int HEIGHT = 320;
 
-        widths[1] = widths[0] + width;
-        heights[1] = heights[0];
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
 
-        widths[2] = widths[1];
-        heights[2] = heights[0] + height;
+    //_______________________________________________________________
+    // set by ZaitzeV16                                           //|
+    // gets current maximum resolution minus the taskbar height   //|
+    int[] curRes = CurrentResolution.getCurrentMaxResolution();   //|
+    WIDTH = curRes[0];                                            //|
+    HEIGHT = curRes[1];                                           //|
+    //______________________________________________________________|
 
-        widths[3] = widths[0];
-        heights[3] = heights[2];
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
+    jFrame.setLocationRelativeTo(null);
 
-        widths[4] = widths[0];
-        heights[4] = heights[0];
+    //_______________________________________________________________
+    // set by ZaitzeV16                                           //|
+    // maximizes the canvas window by default                     //|
+    //      and places it to the middle                           //|
+    jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);               //|
+    //______________________________________________________________|
 
-        graphics.fillPolygon(widths, heights, 4);
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
+
+  static class ImagePanel extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
     }
-
-    // Don't touch the code below
-    //      sorry for that - ZaitzeV16
-    static int WIDTH = 320;
-    static int HEIGHT = 320;
-
-    public static void main(String[] args) {
-        JFrame jFrame = new JFrame("Drawing");
-        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        ImagePanel panel = new ImagePanel();
-
-        //_______________________________________________________________
-        // set by ZaitzeV16                                           //|
-        // gets current maximum resolution minus the taskbar height   //|
-        int[] curRes = CurrentResolution.getCurrentMaxResolution();   //|
-        WIDTH = curRes[0];                                            //|
-        HEIGHT = curRes[1];                                           //|
-        //______________________________________________________________|
-
-        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        jFrame.add(panel);
-        jFrame.setLocationRelativeTo(null);
-
-        //_______________________________________________________________
-        // set by ZaitzeV16                                           //|
-        // maximizes the canvas window by default                     //|
-        //      and places it to the middle                           //|
-        jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);               //|
-        //______________________________________________________________|
-
-        jFrame.setVisible(true);
-        jFrame.pack();
-    }
-
-    static class ImagePanel extends JPanel {
-        @Override
-        protected void paintComponent(Graphics graphics) {
-            super.paintComponent(graphics);
-            mainDraw(graphics);
-        }
-    }
+  }
 }
