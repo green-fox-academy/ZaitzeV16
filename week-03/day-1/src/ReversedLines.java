851b8899ee4530859becc2f21fbfa5a6e2d28d27
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-23 21:39:07 +0200

    [W3-D2] File Manipulation - ReversedLines - completed

---
+++ week-03/day-1/src/ReversedLines.java
@@ -0,0 +1,42 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class ReversedLines {
+
+  public static void main(String[] args) {
+    // Create a method that decrypts reversed-lines.txt
+
+    Path path = Paths.get("./reversed-lines.txt");
+
+    // using file opener function from previous exercise Doubled class
+    ArrayList<String> content = Doubled.getFileContent(path);
+    ArrayList<String> reversedContent = removeDuplicates(content);
+
+    System.out.println(String.join("\n", reversedContent));
+  }
+
+  private static ArrayList<String> removeDuplicates(ArrayList<String> content) {
+    /**
+     * This function takes an ArrayList<String> as parameter,
+     *  iterates through each item and reverses the order of each item
+     *  (using StringBuilder).
+     *
+     *
+     * @param   content ArrayList<String> - content you would like to reverse
+     *                        (String by String, not the order inside content)
+     *
+     * @return ArrayList<String> - reversed order String withing the parameter
+     */
+    
+    ArrayList<String> result = new ArrayList<>();
+    StringBuilder stringBuilder = new StringBuilder();
+
+    for (String line : content) {
+      stringBuilder.append(line);
+      result.add(stringBuilder.reverse().toString());
+      stringBuilder.delete(0, stringBuilder.length());
+    }
+    return result;
+  }
+}
