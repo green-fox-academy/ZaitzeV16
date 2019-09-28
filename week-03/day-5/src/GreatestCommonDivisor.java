97d329af27b8aa905626fe82e726e7f5ade37f05
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-28 18:56:41 +0200

    [W3-D5] Recursion - GreatestCommonDivisor - adjusted variable names to coding style

--- week-03/day-5/src/GreatestCommonDivisor.java
+++ week-03/day-5/src/GreatestCommonDivisor.java
@@ -3,18 +3,18 @@ public class GreatestCommonDivisor {
   public static void main(String[] args) {
     // Find the greatest common divisor of two numbers using recursion.
 
-    int a = 60;
-    int b = 150;
+    int num1 = 60;
+    int num2 = 150;
 
-    int result = recGreatestCommonDivisor(a, b);
-    System.out.println(result);
+    int greatestCommonDivisor = recGreatestCommonDivisor(num1, num2);
+    System.out.println(greatestCommonDivisor);
   }
 
-  private static int recGreatestCommonDivisor(int a, int b) {
-    if (b == 0) {
-      return a;
+  private static int recGreatestCommonDivisor(int num1, int num2) {
+    if (num2 == 0) {
+      return num1;
     } else {
-      return recGreatestCommonDivisor(b, a % b);
+      return recGreatestCommonDivisor(num2, num1 % num2);
     }
   }
 }
