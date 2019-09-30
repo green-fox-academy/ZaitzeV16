a6a65c9a7714e4c0107050bd3a6d7c3f04417dc3
Tamás Haász <tamas.haasz91@gmail.com>
2019-09-30 21:05:37 +0200

    [W4-D2] Using an Interface - Clonable - completed

---
+++ week-04/day-2/src/greenfox/Clonable.java
@@ -0,0 +1,5 @@
+package greenfox;
+
+public interface Clonable {
+  public abstract Clonable clone();
+}
---
+++ week-04/day-2/src/greenfox/Student.java
@@ -0,0 +1,102 @@
+package greenfox;
+
+public class Student extends Person implements Clonable {
+  /*
+  Create a `Student` class that has the same fields and methods as the `Person`
+  class, and has the following additional
+   */
+
+  /*
+  - fields:
+    - `previousOrganization`: the name of the student’s previous company / school
+    - `skippedDays`: the number of days skipped from the course
+   */
+  // private fields
+  private String previousOrganization;
+  private int skippedDays;
+
+    /*
+  The `Student` class has the following constructors:
+
+  - `Student(name, age, gender, previousOrganization)`: beside the given
+    parameters, it sets `skippedDays` to 0
+  - `Student()`: sets `name` to Jane Doe, `age` to 30, `gender` to female,
+    `previousOrganization` to The School of Life, `skippedDays` to 0
+   */
+
+  // constructors
+  public Student() {
+    this(
+        "Jane Doe",
+        30,
+        "female",
+        "The School of Life"
+    );
+  }
+
+  public Student(String name, int age, String gender, String previousOrganization) {
+    super(name, age, gender);
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = 0;
+  }
+
+
+  // getters and setter
+  public String getPreviousOrganization() {
+    return previousOrganization;
+  }
+
+  public void setPreviousOrganization(String previousOrganization) {
+    this.previousOrganization = previousOrganization;
+  }
+
+  public int getSkippedDays() {
+    return skippedDays;
+  }
+
+  public void setSkippedDays(int skippedDays) {
+    this.skippedDays = skippedDays;
+  }
+
+
+  /*
+  - methods:
+    - `getGoal()`: prints out "Be a junior software developer."
+    - `introduce()`: "Hi, I'm `name`, a `age` year old `gender` from
+      `previousOrganization` who skipped `skippedDays` days from the course
+      already."
+    - `skipDays(numberOfDays)`: increases `skippedDays` by `numberOfDays`
+   */
+
+  public void skipDays(int numberOfDays) {
+    this.skippedDays = this.getSkippedDays() + numberOfDays;
+  }
+
+  // override methods
+  @Override
+  public void getGoal() {
+    System.out.println("My goal is: Be a junior software developer.");
+  }
+
+  @Override
+  public void introduce() {
+    System.out.println(
+        "Hi, I'm " + this.getName() +
+            ", a " + this.getAge() +
+            " years old " + this.getGender() +
+            " from " + this.previousOrganization +
+            " who skipped " + this.skippedDays +
+            " days from the course already."
+    );
+  }
+
+  @Override
+  public Student clone() {
+    return new Student(
+        this.getName(),
+        this.getAge(),
+        this.getGender(),
+        this.previousOrganization
+    );
+  }
+}
