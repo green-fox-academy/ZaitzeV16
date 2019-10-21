a011305c359f4652f619ee2fd5e03843c1e896d2
Tamás Haász <tamas.haasz91@gmail.com>
2019-10-21 11:14:50 +0200

    [W6-D1] Streams and Lambdas - Exercise 1 - EvenNumbers - completed

---
+++ week-06/day-1/src/EvenNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EvenNumbers {
+
+  public static void main(String[] args) {
+    /*
+    Write a Stream Expression to get the even numbers from the following list:
+    List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+     */
+    List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+    System.out.println(
+        numbers.stream()
+            .filter(num -> num % 2 == 0)
+            .collect(Collectors.toList())
+    );
+  }
+}
