5ba41d6dedacad0f941fc49383a445387f729a36
Tamás Haász <tamas.haasz91@gmail.com>
2019-10-22 12:06:55 +0200

    [W6-D2] Introduction to Spring - Hello User REST App - completed

--- week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,13 +1,14 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
   @RequestMapping(value = "/greeting")
-  public static Greeting greeting() {
-    return new Greeting(10, "Das ist ein content");
+  public static Greeting greeting(@RequestParam String name) {
+    return new Greeting(10, "Das ist ein content for " + name);
   }
 }
