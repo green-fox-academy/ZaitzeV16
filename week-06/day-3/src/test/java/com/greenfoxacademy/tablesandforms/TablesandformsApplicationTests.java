fcd2e1ed0bf2bdbcb5a7d68226d5847bfadf31fd
Tamás Haász <tamas.haasz91@gmail.com>
2019-10-24 08:54:25 +0200

    [W6-D4] Tables and Forms - initialize spring project

---
+++ week-06/day-3/src/test/java/com/greenfoxacademy/tablesandforms/TablesandformsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.tablesandforms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablesandformsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
