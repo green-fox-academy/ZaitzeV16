6156e2f72e94d8aac7d0e228dfcce0b81a866ed2
Tamás Haász <tamas.haasz91@gmail.com>
2019-10-28 11:56:27 +0100

    [W7-D1] Bank of Simba - showing one account - completed

---
+++ week-07/day-1/bos/src/main/java/com/bankofsimba/bos/controllers/BOSController.java
@@ -0,0 +1,33 @@
+package com.bankofsimba.bos.controllers;
+
+import com.bankofsimba.bos.models.BankAccount;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BOSController {
+  private List<BankAccount> accounts;
+
+  public BOSController() {
+    this.accounts = new ArrayList<>();
+    this.addBankAccount("Simba", 2000, "lion");
+  }
+
+  public void addBankAccount(String name, float balance, String animalType) {
+    this.accounts.add(new BankAccount(name, balance, animalType));
+  }
+
+  @GetMapping(value = {"", "/"})
+  public String showDefault() {
+    return "index";
+  }
+
+  @GetMapping(value = "/show")
+  public String showAccounts(Model model) {
+    model.addAttribute("accounts", this.accounts);
+    return "show_accounts";
+  }
+}
