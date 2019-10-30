993393b194d74121c80ef82b6a934c5e57ce8576
Tamás Haász <tamas.haasz91@gmail.com>
2019-10-30 15:36:05 +0100

    [W7-D3] Using dependencies - Green Fox Class App - add/save student completed

--- week-07/day-3/src/main/java/com/dependency_injection/gfapp/controllers/MainController.java
+++ week-07/day-3/src/main/java/com/dependency_injection/gfapp/controllers/MainController.java
@@ -1,11 +1,11 @@
 package com.dependency_injection.gfapp.controllers;
 
 import com.dependency_injection.gfapp.services.StudentService;
-import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -29,39 +29,14 @@ public class MainController {
     return "gfa_student_list";
   }
 
-//  @GetMapping(value = {"/useful/colored"})
-//  public String showUsefulColored(Model model) {
-//    model.addAttribute("bgColor", this.utilityService.randomColor());
-//    return "useful_colored";
-//  }
-//
-//  @GetMapping(value = {"/useful/email"})
-//  public String showUsefulEmail(Model model, @RequestParam(name = "email") String email) {
-//    HashMap<String, String> data = this.utilityService.validateEmail(email);
-//    model.addAttribute("msg", data.get("msg"));
-//    model.addAttribute("color", data.get("color"));
-//    return "useful_email";
-//  }
-//
-//  @GetMapping(value = {"/useful/caesar/encode"})
-//  public String showCaesarEncoded(Model model,
-//      @RequestParam(name = "number") String number,
-//      @RequestParam(name = "text") String text) {
-//    // multiple params: /useful/caesar/encode?text=yourtext&number=2
-//    model.addAttribute("text", text);
-//    model.addAttribute("parsedText",
-//        this.utilityService.caesar(text, Integer.parseInt(number)));
-//    return "useful_caesar";
-//  }
-//
-//  @GetMapping(value = {"/useful/caesar/decode"})
-//  public String showCaesarDecoded(Model model,
-//      @RequestParam(name = "number") String number,
-//      @RequestParam(name = "text") String text) {
-//    // multiple params: /useful/caesar/encode?text=yourtext&number=2
-//    model.addAttribute("text", text);
-//    model.addAttribute("parsedText",
-//        this.utilityService.caesar(text, -1 * Integer.parseInt(number)));
-//    return "useful_caesar";
-//  }
+  @GetMapping(value = "/gfa/add")
+  public String addStudent() {
+    return "gfa_student_add";
+  }
+
+  @PostMapping(value = "/gfa/save")
+  public String saveStudent(@RequestParam(name = "name") String student_name) {
+    this.studentService.save(student_name);
+    return "redirect:/gfa/list";
+  }
 }
