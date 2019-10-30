9fa5e514415088754a03e6ce2456fc1d5c146896
Tamás Haász <tamas.haasz91@gmail.com>
2019-10-30 12:18:57 +0100

    [W7-D3] Using dependencies - Hello DI project - completed

--- week-07/day-3/src/main/java/com/dependency_injection/di/DiApplication.java
+++ week-07/day-3/src/main/java/com/dependency_injection/di/DiApplication.java
@@ -1,13 +1,27 @@
 package com.dependency_injection.di;
 
+import com.dependency_injection.di.services.PrinterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiApplication {
+public class DiApplication implements CommandLineRunner {
+
+  private PrinterService printerService;
+
+  @Autowired
+  public DiApplication(PrinterService printerService) {
+    this.printerService = printerService;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(DiApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+    this.printerService.log("hello");
+  }
 }
