c8bc047f7783774371c28fdd5d63805fc27a0287
Tamás Haász <tamas.haasz91@gmail.com>
2019-11-05 12:17:39 +0100

    [W8-D2] DataBase Integration - Connect to MySQL - init project

---
+++ week-08/day-2/connectMySQL/src/main/java/com/greenfox/mysql/connect_mysql/ConnectMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.mysql.connect_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectMysqlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConnectMysqlApplication.class, args);
+  }
+
+}
---
+++ week-08/day-2/connectMySQL/src/test/java/com/greenfox/mysql/connect_mysql/ConnectMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfox.mysql.connect_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectMysqlApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
