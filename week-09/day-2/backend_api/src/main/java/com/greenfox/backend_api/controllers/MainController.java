e08c99494acda524fc1ff9a4cc8577262a050892
Tamás Haász <tamas.haasz91@gmail.com>
2019-11-08 15:00:13 +0100

    [W9-D2] Backend API - Doubling - completed

--- week-09/day-2/backend_api/src/main/java/com/greenfox/backend_api/controllers/MainController.java
+++ week-09/day-2/backend_api/src/main/java/com/greenfox/backend_api/controllers/MainController.java
@@ -1,16 +1,32 @@
 package com.greenfox.backend_api.controllers;
 
+import java.util.HashMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class MainController {
 
-  @GetMapping(name = "/")
+  // region GetMappings
+  @GetMapping(value = "/")
   public ModelAndView index() {
     ModelAndView modelAndView = new ModelAndView();
     modelAndView.setViewName("index");
     return modelAndView;
   }
+
+  @GetMapping(value = "/doubling")
+  public HashMap<String, Object> doubling(
+      @RequestParam(name = "input", required = false) Integer input) {
+    HashMap<String, Object> result = new HashMap<>();
+    if (input == null) {
+      result.put("error", "Please provide an input!");
+    } else {
+      result.put("received", input);
+      result.put("result", input * 2);    }
+    return result;
+  }
+  // endregion GetMappings
 }
