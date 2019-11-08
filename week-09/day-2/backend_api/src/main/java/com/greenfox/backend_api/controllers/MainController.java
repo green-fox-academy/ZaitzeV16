1e034fd3d672121a03c63bf3daf3c291c961d555
Tamás Haász <tamas.haasz91@gmail.com>
2019-11-08 15:23:47 +0100

    [W9-D2] Backend API - Greeter - completed

--- week-09/day-2/backend_api/src/main/java/com/greenfox/backend_api/controllers/MainController.java
+++ week-09/day-2/backend_api/src/main/java/com/greenfox/backend_api/controllers/MainController.java
@@ -1,6 +1,8 @@
 package com.greenfox.backend_api.controllers;
 
 import java.util.HashMap;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +29,26 @@ public class MainController {
       result.put("received", input);
       result.put("result", input * 2);    }
     return result;
+  }
+
+  @GetMapping(value = "/greeter")
+  public ResponseEntity greeter(
+      @RequestParam(name = "name", required = false) String name,
+      @RequestParam(name = "title", required = false) String title) {
+    HashMap<String, Object> result = new HashMap<>();
+    if (name == null && title == null) {
+      result.put("error", "Please provide a name and a title!");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    } else if (name == null) {
+      result.put("error", "Please provide a name!");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    } else if (title == null) {
+      result.put("error", "Please provide a title!");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    } else {
+      result.put("welcome_message", "Oh, hi there " + name + ", my dear "+ title + "!");
+      return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
   }
   // endregion GetMappings
 }
