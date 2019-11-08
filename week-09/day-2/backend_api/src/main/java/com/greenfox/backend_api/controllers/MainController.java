d7fe485ef9a5ed2df433f7a627bcdaeb5857613a
Tamás Haász <tamas.haasz91@gmail.com>
2019-11-08 14:03:27 +0100

    [W9-D2] Backend API - Fronted - completed

---
+++ week-09/day-2/backend_api/src/main/java/com/greenfox/backend_api/controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.greenfox.backend_api.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+  @GetMapping(name = "/")
+  public String index() {
+    return "index";
+  }
+}
