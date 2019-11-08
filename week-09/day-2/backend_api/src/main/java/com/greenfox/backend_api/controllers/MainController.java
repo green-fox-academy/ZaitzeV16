7b77586e1306110a0dd3772a3e9e1167f2494116
Tamás Haász <tamas.haasz91@gmail.com>
2019-11-08 16:24:02 +0100

    [W9-D2] Backend API - Do until - completed

--- week-09/day-2/backend_api/src/main/java/com/greenfox/backend_api/controllers/MainController.java
+++ week-09/day-2/backend_api/src/main/java/com/greenfox/backend_api/controllers/MainController.java
@@ -1,10 +1,13 @@
 package com.greenfox.backend_api.controllers;
 
 import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,7 +31,8 @@ public class MainController {
       result.put("error", "Please provide an input!");
     } else {
       result.put("received", input);
-      result.put("result", input * 2);    }
+      result.put("result", input * 2);
+    }
     return result;
   }
 
@@ -47,7 +51,7 @@ public class MainController {
       result.put("error", "Please provide a title!");
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
     } else {
-      result.put("welcome_message", "Oh, hi there " + name + ", my dear "+ title + "!");
+      result.put("welcome_message", "Oh, hi there " + name + ", my dear " + title + "!");
       return ResponseEntity.status(HttpStatus.OK).body(result);
     }
   }
@@ -65,4 +69,53 @@ public class MainController {
     }
   }
   // endregion GetMappings
+
+
+  // region PostMappings
+  @PostMapping(value = "/dountil/{action}")
+  public ResponseEntity doUntil(
+      @PathVariable(name = "action", required = false) String action,
+      @RequestBody(required = false) Map<String, ?> input) {
+
+    HashMap<String, Object> result = new HashMap<>();
+
+    if (action == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    } else if (input == null || !input.containsKey("until")) {
+      result.put("error", "Please provide a number!");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    } else {
+
+      if ("factor".equals(action)) {
+        int num = Integer.parseInt(String.valueOf(input.get("until")));
+        result.put("result", factorioRecursive(num));
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+
+      } else if ("sum".equals(action)) {
+        int num = Integer.parseInt(String.valueOf(input.get("until")));
+        result.put("result", sumRecursive(num));
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+      }
+    }
+    return ResponseEntity.status(HttpStatus.OK).body(result);
+  }
+  // endregion PostMappings
+
+  // region HelperFunction
+  private static long factorioRecursive(int num) {
+    if (num == 1) {
+      return 1;
+    } else {
+      return num * (factorioRecursive(num - 1));
+    }
+  }
+
+  private static int sumRecursive(int n) {
+    if (n == 1) {
+      return 1;
+    } else {
+      return sumRecursive(n - 1) + n;
+    }
+  }
+  // endregion HelperFunction
 }
