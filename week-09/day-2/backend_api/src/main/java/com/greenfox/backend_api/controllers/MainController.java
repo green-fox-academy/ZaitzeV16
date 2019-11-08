6bcccea4efa7fb5b4be78ea8b5e9aca842f52588
Tamás Haász <tamas.haasz91@gmail.com>
2019-11-08 14:08:54 +0100

    [W9-D2] Backend API - Fronted - changed back to RestController annotation with ModelAndView

--- week-09/day-2/backend_api/src/main/java/com/greenfox/backend_api/controllers/MainController.java
+++ week-09/day-2/backend_api/src/main/java/com/greenfox/backend_api/controllers/MainController.java
@@ -1,13 +1,16 @@
 package com.greenfox.backend_api.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class MainController {
 
   @GetMapping(name = "/")
-  public String index() {
-    return "index";
+  public ModelAndView index() {
+    ModelAndView modelAndView = new ModelAndView();
+    modelAndView.setViewName("index");
+    return modelAndView;
   }
 }
