482bb3a0bbe2515d202621c5662c134262b657ab
Tamás Haász <tamas.haasz91@gmail.com>
2019-11-25 16:46:04 +0100

    [W9-D5] Rest Test - Yondu - 4 tests created

--- week-09/day-5/src/test/java/com/greenfox/rest_test/controllers/GuardianControllerTest.java
+++ week-09/day-5/src/test/java/com/greenfox/rest_test/controllers/GuardianControllerTest.java
@@ -46,4 +46,44 @@ public class GuardianControllerTest {
         .andExpect(jsonPath("$.error", is("I am Groot!")));
   }
   // endregion getGroot
+
+
+  // region getYondu
+  @Test
+  public void getYondu_WithBothParameters_StatusAndResponseIsProper() throws Exception {
+    this.mockMvc.perform(get("/yondu?distance=100.0&time=10.0"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.distance", is(100.0)))
+        .andExpect(jsonPath("$.time", is(10.0)))
+        .andExpect(jsonPath("$.speed", is(10.0)));
+  }
+
+  @Test
+  public void getYondu_WithDistanceParameter_StatusAndResponseIsProper() throws Exception {
+    this.mockMvc.perform(get("/yondu?distance=100.0"))
+        .andExpect(status().isBadRequest())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.error",
+            is("No sufficient number of parameters provided!")));
+  }
+
+  @Test
+  public void getYondu_WithTimeParameter_StatusAndResponseIsProper() throws Exception {
+    this.mockMvc.perform(get("/yondu?time=10.0"))
+        .andExpect(status().isBadRequest())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.error",
+            is("No sufficient number of parameters provided!")));
+  }
+
+  @Test
+  public void getYondu_WithoutParameters_StatusAndResponseIsProper() throws Exception {
+    this.mockMvc.perform(get("/yondu"))
+        .andExpect(status().isBadRequest())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.error",
+            is("No sufficient number of parameters provided!")));
+  }
+  // endregion getYondu
 }
