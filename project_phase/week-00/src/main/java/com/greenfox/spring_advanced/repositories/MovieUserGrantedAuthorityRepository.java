5c62f7506c1c4b06f1e329d5cda4e85719103642
Tamás Haász <tamas.haasz91@gmail.com>
2019-12-05 17:06:24 +0100

    [PROJECT_PHASE-W-0] Spring advanced - added MovieUserGrantedAuthority repository

---
+++ project_phase/week-00/src/main/java/com/greenfox/spring_advanced/repositories/MovieUserGrantedAuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.greenfox.spring_advanced.repositories;
+
+import com.greenfox.spring_advanced.models.entities.MovieUserGrantedAuthority;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieUserGrantedAuthorityRepository extends
+    CrudRepository<MovieUserGrantedAuthority, Long> {
+
+}
