ea2ea3877c39d6f0d856c81f7d575a8c41eb16d3
Tamás Haász <tamas.haasz91@gmail.com>
2019-12-05 17:15:36 +0100

    [PROJECT_PHASE-W-0] Spring advanced - added/fixed MovieUserGranted(repository/service/serviceImpl)

--- project_phase/week-00/src/main/java/com/greenfox/spring_advanced/repositories/MovieUserGrantedAuthorityRepository.java
+++ project_phase/week-00/src/main/java/com/greenfox/spring_advanced/repositories/MovieUserGrantedAuthorityRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface MovieUserGrantedAuthorityRepository extends
     CrudRepository<MovieUserGrantedAuthority, Long> {
 
+  public MovieUserGrantedAuthority findByAuthority(String authority);
 }
